faacf3e73097f603a3952d9f450eaf21a245e6be
Cawa <777.777.1234@mail.ru>
2016-03-15 11:48:43 +0300

    1st def completed

--- cat/cat.c
+++ cat/cat.c
@@ -1,13 +1,43 @@
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
 	int BUFF_SIZE = 1024;
 	char buff[BUFF_SIZE];
-	int status = 1;
-	while (status > 0) {
-		status = read(0, buff, BUFF_SIZE);
-		if (status > 0) { 
-			int j = write(1, buff, status);
+	int status_in = 1;
+	int length = 1;
+	int fdin = 0;
+	if (argc == 2) {
+		fdin = open(argv[1]);
+		if (fdin < 0) {
+			status_in = -1;
+		} 
+	}
+	while (status_in > 0) {
+		status_in = read(fdin, buff, BUFF_SIZE);
+		if (status_in > 0) {
+			length = status_in;
+			while (length > 0) {
+				int status_out = write(1, buff, length);
+				if (status_out >= 0) {
+					length -= status_out;
+				} else { 				
+					if (errno == EINTR) {
+						status_out = 1;
+					} else {
+						status_in = -1;
+						close(fdin);
+						return 1;
+					}
+				}
+			}
+		}
+		if (errno == EINTR) {
+			status_in = 1;	
+		} else {
+			status_in = -1;
+			close(fdin);
+			return 1;
 		}
 	}
 	return 0;
